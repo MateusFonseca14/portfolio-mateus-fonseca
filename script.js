9fad35cf71dfdcad6e8c7349b127bca65918a0c7
Mateus Fonseca <175280307+MateusFonseca14@users.noreply.github.com>
2026-05-11 20:41:50 -0300

    Style Section Sobre / Contato

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.getElementById("btnCopiar").addEventListener("click", function () {
 
   // Salva o estilo original para voltar depois
   const bgOriginal = btn.style.backgroundColor;
+  const paddingLeftOriginal = btn.style.paddingLeft;
+  const paddingRightOriginal = btn.style.paddingRight;
   const corOriginal = btn.style.color;
   const bordersizeOriginal = btn.style.border;
   const borderstyleOriginal = btn.style.borderStyle;
@@ -20,17 +22,21 @@ document.getElementById("btnCopiar").addEventListener("click", function () {
       textoBtn.innerText = "E-mail copiado!";
 
       // 2. Muda o fundo e a cor do texto/ícone
-      btn.style.backgroundColor = "#28a745"; // Verde (exemplo)
-      btn.style.color = "#ffffff"; // Branco
-      btn.style.border = "3px";
+      btn.style.backgroundColor = "#9072d4"; // Verde (exemplo)
+      btn.style.paddingLeft = "12px"
+      btn.style.paddingRight = "11.9px";
+      btn.style.color = "#383838"; // Branco
+      btn.style.border = "4px";
       btn.style.borderStyle = "solid";
-      btn.style.borderColor = "#FFFF00";
-      svgPath.style.fill = "#FFFF00"; // Exemplo: ícone amarelo
+      btn.style.borderColor = "#383838";
+      svgPath.style.fill = "#c4c4c4"; // Exemplo: ícone amarelo
 
       // Volta ao estado original após 2 segundos
       setTimeout(() => {
         textoBtn.innerText = textoOriginal;
         btn.style.backgroundColor = bgOriginal;
+        btn.style.paddingLeft = paddingLeftOriginal;
+        btn.style.paddingRight = paddingRightOriginal;
         btn.style.color = corOriginal;
         btn.style.border = bordersizeOriginal;
         btn.style.borderStyle = borderstyleOriginal;
